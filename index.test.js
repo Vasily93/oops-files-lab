eb42ffdb05071f62842bb90ffd6d74edce73e981
Vasily93 <markovavasily@gmail.com>
2019-06-25 17:02:37 -0700

    init

---
+++ index.test.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+// const word = ['cup', 'pillow', 'couch', 'table', 'coffee'];
+
+
+
+
+
+
+// fs.writeFile(__dirname + 'random-number.txt', 'random-word', err => {
+//     if (err) return console.error(err);
+// })
